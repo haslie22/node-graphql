da67a816578fa0c5f324b9acdfa33ddc41f7c5ae
Lizaveta <lizadondysh@icloud.com>
2024-03-06 00:09:48 +0200

    feat: implement rule

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, gqlSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, validate, parse } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -15,6 +16,11 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     async handler(req) {
       const { query, variables } = req.body;
       const { prisma } = fastify;
+
+      const depthErrors = validate(gqlSchema, parse(query), [depthLimit(5)]);
+      if (depthErrors.length) {
+        return { data: null, errors: depthErrors };
+      }
 
       return graphql({
         schema: gqlSchema,
